9cb1eb590332e357f7ff8ad13f400e501d8d9596
swathimeenal <swathimeenal1994@gmail.com>
2024-02-19 12:42:28 +0530

    update

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-import express from 'express';
-import fs from 'fs';
-import {format} from 'date-fns';
-import path from 'path';
+const PORT=8000
+const fs= require('fs')
+const express= require('express')
+const app=express()
 
-const PORT = 8000;
-const app = express();
-
-app.use(express.json());
-
-app.get('/',(req,res)=>{
-    try {
-        const today = format (new Date(),'dd-MM-yyyy-HH-mm-ss');
-        const filePath = path.join('Current_TimeStamp',`${today}.txt`);
-
-        fs.writeFileSync(filePath, today, 'utf8');
-        const data = fs.readFileSync(filePath,'utf8');
-
-        res.status(200).send(data);
-
-    } catch(error){
-        console.error(error);
-        res.status(500).send('Internal server error');
-
-    }
+app.use('/',(req,res)=>{
+  try {
+    let today=new Date().getTime()
+    fs.writeFileSync(`Current_TimeStamp/${today}.txt`,`${today}`,'utf8')
+    let data=fs.readFileSync(`Current_TimeStamp/${today}.txt`,'utf8')
+    res.send(data)
+    } 
+  catch (error) {  
+  }
 })
-
-//! New endpoint to retrieve all text file in a folder
-
-app.get('/getTextFiles',(req,res=>{
-    const folderPath = 'Current_TimeStamp';
-
-    fs.readdir(folderPath,(error, files)=>{
-        if(error){
-            console.error(error);
-            res.status(500).send('An error occured while listing the files from the directory');
-        }
-        else{
-            const textFiles = files.filter((file)=>path.extname(file)==='.txt');
-            res.status(200).json(textFiles);
-
-        }
-    })
-}))
-
-app.listen(PORT,()=>console.log(`App is listening to the port ${PORT}`));
+app.listen(PORT,()=>console.log(`Server listening at http://localhost:${PORT}`))
